16f8bfd3ae56bd6449592a1de6d74a0ee00d7a3e
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2022-02-05 22:03:50 -0300

    Removing files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AirportModule } from './airport/airport.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AirportModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats')
-    .setDescription('The cats API description')
+    .setTitle('Airport')
+    .setDescription('The Airport API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
